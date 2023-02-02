9363a208b10021885ec15cc85820b288ff2897d4
Somenath Choudhury <81456073+somenath203@users.noreply.github.com>
2023-02-02 21:01:55 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Tippy from '@tippyjs/react';
 const navbar = () => {
   return (
     <nav className="bg-slate-800 h-20 flex items-center justify-between p-12">
-      <p className="text-xl md:text-3xl lg:text-4xl font-bold font-mono tracking-widest mt-6 text-white">ShortMyURL</p>
+      <p className="text-xl md:text-3xl lg:text-4xl font-bold font-mono tracking-wide mt-6 text-white">ShortMyURL</p>
       <Tippy content={<span>ShortMyURL Github Repo</span>}>
         <p className="text-2xl lg:text-5xl mt-6 text-white">
           <a href="https://github.com/somenath203/Mern-Stack-URL-Shortener-Frontend" target='_blank' rel="noreferrer">
